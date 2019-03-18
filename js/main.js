53ff7157e3f6268ec4bc4d79bfda0f34505901f7
morphIsmail <ismail_2016@mail.ru>
2019-03-18 12:10:44 +0300

    Портфолио завершенное

--- js/main.js
+++ js/main.js
@@ -1,33 +1,15 @@
-$(document).ready(function() {
-   $('.button[filter="wd"]').click(function(){
-      if($(this).attr('val')=='off') {
-         $('.button[filter]').attr('val', 'off');
-         $(this).attr('val', 'on');
+$(document).ready(function () {
+   $('.button[filter]').click(function () {
+      if ($(this).attr('val') == 'off') {
+         $('.button[filter]').attr('val', 'off').removeClass('focused');
+         $(this).attr('val', 'on').addClass('focused');
          $('.filter > div').hide(300);
-         $('.filter > div[filter="wd"]').show(300);
+         $('.filter > div[filter=' + $(this).attr('filter') + ']').show(300);
+         if ($(this).attr('filter') == 'all') {
+            $('.button[filter]').attr('val', 'off').removeClass('focused');
+            $(this).attr('val', 'on').addClass('focused');
+            $('.filter > div').show(300);
+         }
       }
-   });
-   $('.button[filter="ud"]').click(function(){
-      if($(this).attr('val')=='off') {
-         $('.button[filter]').attr('val', 'off');
-         $(this).attr('val', 'on');
-         $('.filter > div').hide(300);
-         $('.filter > div[filter="ud"]').show(300);
-      }
-   });
-   $('.button[filter="moc"]').click(function(){
-      if($(this).attr('val')=='off') {
-         $('.button[filter]').attr('val', 'off');
-         $(this).attr('val', 'on');
-         $('.filter > div').hide(300);
-         $('.filter > div[filter="moc"]').show(300);
-      }
-   });
-   $('.button[filter="all"]').click(function(){
-      if($(this).attr('val')=='off') {
-         $('.button[filter]').attr('val', 'off');
-         $(this).attr('val', 'on');
-         $('.filter > div').show(300);
-      }
-   });
-});
+   })
+})
