f6249d6ab3a28779c41559e2b1ae4543bbb3f52b
morphIsmail <ismail_2016@mail.ru>
2019-03-31 20:42:05 +0300

    Команда

--- js/main.js
+++ js/main.js
@@ -12,4 +12,12 @@ $(document).ready(function () {
          }
       }
    })
+   $('.multiple-items').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      arrows: false,
+      dots: true,
+      dotsClass: 'dots-style'
+   });
 })
