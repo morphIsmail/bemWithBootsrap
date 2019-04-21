b2c4a052a2859e613376d408561550005f11afcc
morphIsmail <ismail_2016@mail.ru>
2019-04-21 14:03:30 +0300

    адаптив под планшеты

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+   /* Портфолио */
    $('.button[filter]').click(function () {
       if ($(this).attr('val') == 'off') {
          $('.button[filter]').attr('val', 'off').removeClass('focused');
@@ -11,13 +12,59 @@ $(document).ready(function () {
             $('.filter > div').show(300);
          }
       }
-   })
+   });
+   /* Слайдер */
    $('.multiple-items').slick({
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 3,
       arrows: false,
       dots: true,
-      dotsClass: 'dots-style'
+      dotsClass: 'dots-style',
+      responsive: [{
+         breakpoint: 1024,
+         settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+         }
+      }]
+   });
+   /* Меню */
+   $('ul.menu a[href^="#"').click(function () {
+      var target = $(this).attr('href');
+      $('html, body').animate({
+         scrollTop: $(target).offset().top
+      }, 500);
+      $('ul.menu a[href^="#"').css({
+         'color': '#212121'
+      });
+      $(this).css({
+         'color': '#004bee'
+      });
+      return false;
+   });
+   /* Выпадающее меню */
+   $('.menu-icon').click(function(){
+      $('nav').slideToggle(500);
+      $('ul').css({
+         'display':'flex', 'flex-direction':'column'
+      })
+      if($('.menu-icon').html()=='<i class="fas fa-bars"></i>') {
+         $(this).html('<i class="fas fa-times"></i>')
+      } else {
+         $(this).html('<i class="fas fa-bars"></i>')
+      }
+   })
+   /* Кнопка наверх */
+   $(window).scroll(function () {
+      if ($(this).scrollTop() != 0)
+         $('#toTop').fadeIn();
+      else
+         $('#toTop').fadeOut();
+   });
+   $('#toTop').click(function () {
+      $('body,html').animate({
+         scrollTop: 0
+      }, 800);
    });
 })
