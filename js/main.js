101651e2a5ef05ee2c8804fc96f168a44e3abdbd
morphIsmail <ismail_2016@mail.ru>
2019-04-29 10:36:45 +0300

    Адаптив под Смартфоны

--- js/main.js
+++ js/main.js
@@ -27,6 +27,12 @@ $(document).ready(function () {
             slidesToShow: 2,
             slidesToScroll: 2,
          }
+      },{
+         breakpoint: 480,
+         settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+         }
       }]
    });
    /* Меню */
@@ -46,7 +52,7 @@ $(document).ready(function () {
    /* Выпадающее меню */
    $('.menu-icon').click(function(){
       $('nav').slideToggle(500);
-      $('ul').css({
+      $('ul.menu').css({
          'display':'flex', 'flex-direction':'column'
       })
       if($('.menu-icon').html()=='<i class="fas fa-bars"></i>') {
